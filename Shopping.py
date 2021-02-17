9843d721a6813aca6fbf8bad27e5996e92bef881
Whitetiger094 <75635340+Whitetiger094@users.noreply.github.com>
2021-02-17 08:10:42 -0600

    Add files via upload

---
+++ Shopping.py
@@ -0,0 +1,3 @@
+#Project: Create python file that orders products and writes them to a file
+#How: Using GUI and buttons, products from a set list will be chosen and in the end shown and added together to show final price(with tax)
+#Who: Kunal A, Trisha M, Sri J, and Shane
